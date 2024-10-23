830a6d195e10c9390bcb90b2682b251c57935ae5
dimuthx353 <dimuthx353@gmail.com>
2024-10-23 18:09:10 +0530

    almost done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
-var url =
-  'https://newsapi.org/v2/top-headlines?country=us&apiKey=72f827510e6a46cb8736388e22a252e0';
+let url = '';
+const baseUrl = 'https://newsapi.org/v2/';
+const apiKey = '&apiKey=72f827510e6a46cb8736388e22a252e0';
 
 const newsEl = document.querySelector('.news');
 
@@ -13,11 +14,24 @@ async function dataReq(url) {
   }
 }
 
-dataReq(url).then((data) => {
-  const news = data.articles;
-  news.forEach((element) => {
-    if (element.title !== '[Removed]' && element.description !== null) {
-      const template = `
+function updateNews(type) {
+  newsEl.innerHTML = '';
+
+  if (type === 'us') {
+    url = `${baseUrl}top-headlines?country=us${apiKey}`;
+  } else if (type === 'business') {
+    url = `${baseUrl}/top-headlines?country=us&category=business${apiKey}`;
+  } else if (type === 'cryptocurrency') {
+    url = `${baseUrl}/everything?q=crypto&sortBy=publishdAt${apiKey}`;
+  } else if (type === 'techcrunch') {
+    url = `${baseUrl}/top-headlines?sources=techcrunch${apiKey}`;
+  }
+
+  dataReq(url).then((data) => {
+    const news = data.articles;
+    news.forEach((element) => {
+      if (element.title !== '[Removed]' && element.description !== null) {
+        const template = `
        <div class=" p-3 news-card d-flex align-items-center shadow">
           <img src="${element.urlToImage}" alt="News Image" class="news-image" />
           <div class="news-content">
@@ -32,7 +46,8 @@ dataReq(url).then((data) => {
           </div>
       </div>`;
 
-      newsEl.innerHTML += template;
-    }
+        newsEl.innerHTML += template;
+      }
+    });
   });
-});
+}
